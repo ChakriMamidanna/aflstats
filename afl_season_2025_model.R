93c038afad266f6b63ec3b7564ec4be0246d04b4
chakrimamidanna <chakrimamidanna@gmail.com>
2025-07-31 21:06:33 +1000

    rd 21

--- afl_season_2025_model.R
+++ afl_season_2025_model.R
@@ -2,7 +2,7 @@ library(fitzRoy)
 library(tidyverse)
 library(zoo)
 library(gmailr)
-rd = 20
+rd = 21
 # pred_bits_opti
 # Load AFL data using fitzRoy
 # data <- fitzRoy::fetch_results_afltables(2015, 2022)
